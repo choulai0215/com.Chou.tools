3645ce8e97f686c609cb0598bc8f78d4bae66a8c
choulai0215 <choulai0215@gmail.com>
2024-11-07 12:42:11 +0800

    Initial commit

---
+++ RunTime/SceneControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Chou.Tools
+{
+    /// <summary>
+    /// 場景控制.提供載入場景與退出遊戲規範
+    /// </summary>
+
+    public class SceneControl : MonoBehaviour
+    {
+
+        /// <summary>
+        /// 載入場景
+        /// </summary>
+        /// <param name="sceneName">要載入的場景名稱</param>
+        public void LoadScene(string sceneName)
+        {
+            //場景管理器的載入場景(場景名稱)
+            SceneManager.LoadScene(sceneName);
+        }
+
+
+        /// <summary>
+        /// 退出遊戲
+        /// </summary>
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
